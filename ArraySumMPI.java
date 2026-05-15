273b7a9b5451967b57279d110f98a51ff15ba2b6
ls501 <dikshaarsule16@gmail.com>
2026-05-15 08:26:08 +0530

    Update ArraySumMPI.java

--- ArraySumMPI.java
+++ ArraySumMPI.java
@@ -1,4 +1,67 @@
+
+
 import mpi.*;
+
+public class ArraySumMPI {
+
+    public static void main(String[] args) throws Exception {
+
+        MPI.Init(args);
+
+        int rank = MPI.COMM_WORLD.Rank();
+        int size = MPI.COMM_WORLD.Size();
+
+        int[] array = {1,2,3,4,5,6,7,8,9,10};
+
+        int n = array.length;
+
+        int localSum = 0;
+
+        int startIndex = rank * (n / size);
+
+        int endIndex;
+
+        if(rank == size - 1) {
+            endIndex = n;
+        } else {
+            endIndex = startIndex + (n / size);
+        }
+
+        // Calculate local sum
+        for(int i = startIndex; i < endIndex; i++) {
+            localSum += array[i];
+        }
+
+        int[] globalSum = new int[1];
+
+        // Reduce all local sums
+        MPI.COMM_WORLD.Reduce(
+                new int[]{localSum},
+                0,
+                globalSum,
+                0,
+                1,
+                MPI.INT,
+                MPI.SUM,
+                0
+        );
+
+        // Print final sum
+        if(rank == 0) {
+            System.out.println("Global Sum = " + globalSum[0]);
+        }
+
+        MPI.Finalize();
+    }
+}
+
+
+
+
+
+
+
+//import mpi.*;
 
 public class ArraySumMPI {
 
